ff80b9d4afa78f33087fae8d311499f880846569
Victor Vianna <vitorvianna27@gmail.com>
2023-07-01 09:41:59 -0300

    ✨ feat: Initial commit

---
+++ src/br/com/ebac/wrappers/WrappersCasting.java
@@ -0,0 +1,12 @@
+package br.com.ebac.wrappers;
+
+public class WrappersCasting {
+
+    public static void main(String[] args) {
+        double i = 30.00d;
+        System.out.println(i);
+        Double newValue = i;
+        System.out.println(newValue);
+    }
+
+}
